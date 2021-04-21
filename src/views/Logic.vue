e2ae903c49845ec139100933407df31f77d5fd99
mccrush <mccrush2027@gmail.com>
2021-04-21 20:07:09 +0700

    Settinga page Logic

--- src/views/Logic.vue
+++ src/views/Logic.vue
@@ -2,7 +2,96 @@
   <div class="row">
     <div class="col-12 pt-3">
       <h2>Логика</h2>
+      <hr />
+      <h4>Порядок выполнения логических операций</h4>
+      <div class="row">
+        <div class="col-12 col-sm-6">
+          <a
+            data-fancybox="gallery"
+            href="img/logic/por1.jpg"
+            class="card h-100 mt-2 mt-sm-0"
+          >
+            <img
+              src="img/logic/por1.jpg"
+              alt="Порядок выполнения логических операций"
+              class="w-100"
+            />
+          </a>
+        </div>
+        <div class="col-12 col-sm-6 mt-2 mt-sm-0">
+          <a
+            data-fancybox="gallery"
+            href="img/logic/por2.png"
+            class="card h-100 mt-2 mt-sm-0"
+          >
+            <img
+              src="img/logic/por2.png"
+              alt="Порядок выполнения логических операций"
+              class="w-100"
+            />
+          </a>
+        </div>
+      </div>
+
+      <hr />
+      <h4>Таблицы истинности</h4>
+      <div class="row">
+        <div class="col-12">
+          <a
+            data-fancybox="gallery"
+            href="img/logic/tabist.png"
+            class="card h-100 mt-2 mt-sm-0"
+          >
+            <img
+              src="img/logic/tabist.png"
+              alt="Таблицы истинности"
+              class="w-100"
+            />
+          </a>
+        </div>
+        <div class="col-12"></div>
+        <div class="col-12"></div>
+      </div>
+
+      <hr />
+      <h4>Законы логики</h4>
+      <div class="row">
+        <div class="col-12 col-sm-6">
+          <a
+            data-fancybox="gallery"
+            href="img/logic/zakony_logiki.png"
+            class="card h-100 mt-2 mt-sm-0"
+          >
+            <img
+              src="img/logic/zakony_logiki.png"
+              alt="Законы логики"
+              class="w-100"
+            />
+          </a>
+        </div>
+        <div class="col-12 col-sm-6 mt-2 mt-sm-0">
+          <a
+            data-fancybox="gallery"
+            href="img/logic/zakony.jpg"
+            class="card h-100 mt-2 mt-sm-0"
+          >
+            <img src="img/logic/zakony.jpg" alt="Законы логики" class="w-100" />
+          </a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+a.card {
+  border: 1px solid #dee2e6;
+  transition: 0.3s ease-in-out;
+}
+
+a.card:hover {
+  border-color: #fff;
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+}
+</style>
 
