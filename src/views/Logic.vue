a26d5294b83e4043f3cfb114b1683279df5a83b6
mccrush <mccrush2027@gmail.com>
2021-04-21 20:11:01 +0700

    Replace rows

--- src/views/Logic.vue
+++ src/views/Logic.vue
@@ -2,36 +2,6 @@
   <div class="row">
     <div class="col-12 pt-3">
       <h2>Логика</h2>
-      <hr />
-      <h4>Порядок выполнения логических операций</h4>
-      <div class="row">
-        <div class="col-12 col-sm-6">
-          <a
-            data-fancybox="gallery"
-            href="img/logic/por1.jpg"
-            class="card h-100 mt-2 mt-sm-0"
-          >
-            <img
-              src="img/logic/por1.jpg"
-              alt="Порядок выполнения логических операций"
-              class="w-100"
-            />
-          </a>
-        </div>
-        <div class="col-12 col-sm-6 mt-2 mt-sm-0">
-          <a
-            data-fancybox="gallery"
-            href="img/logic/por2.png"
-            class="card h-100 mt-2 mt-sm-0"
-          >
-            <img
-              src="img/logic/por2.png"
-              alt="Порядок выполнения логических операций"
-              class="w-100"
-            />
-          </a>
-        </div>
-      </div>
 
       <hr />
       <h4>Таблицы истинности</h4>
@@ -76,6 +46,37 @@
             class="card h-100 mt-2 mt-sm-0"
           >
             <img src="img/logic/zakony.jpg" alt="Законы логики" class="w-100" />
+          </a>
+        </div>
+      </div>
+
+      <hr />
+      <h4>Порядок выполнения логических операций</h4>
+      <div class="row">
+        <div class="col-12 col-sm-6">
+          <a
+            data-fancybox="gallery"
+            href="img/logic/por1.jpg"
+            class="card h-100 mt-2 mt-sm-0"
+          >
+            <img
+              src="img/logic/por1.jpg"
+              alt="Порядок выполнения логических операций"
+              class="w-100"
+            />
+          </a>
+        </div>
+        <div class="col-12 col-sm-6 mt-2 mt-sm-0">
+          <a
+            data-fancybox="gallery"
+            href="img/logic/por2.png"
+            class="card h-100 mt-2 mt-sm-0"
+          >
+            <img
+              src="img/logic/por2.png"
+              alt="Порядок выполнения логических операций"
+              class="w-100"
+            />
           </a>
         </div>
       </div>
