80f6028bdad8e6a854cf359231d7d1c5f4243501
mccrush <mccrush2027@gmail.com>
2021-04-22 09:34:33 +0700

    Update page Step 2

--- src/views/Step.vue
+++ src/views/Step.vue
@@ -2,42 +2,48 @@
   <div class="row">
     <div class="col-12 pt-4">
       <h2>Степени двойки</h2>
-      <input
-        class="form-range w-100"
-        type="range"
-        min="0"
-        max="32"
-        step="1"
-        v-model.number="step"
-      />
-      <table class="table table-bordered table-striped">
-        <thead>
-          <tr>
-            <th class="w-50">n</th>
-            <th>2<sup>n</sup></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>0</td>
-            <td>1</td>
-          </tr>
-          <tr v-for="n in step" :key="n + 'id'">
-            <td>{{ n }}</td>
-            <td>{{ Math.pow(2, n).toLocaleString() }}</td>
-          </tr>
-        </tbody>
-      </table>
+      <div class="row">
+        <div class="col-12 col-sm-6">
+          <table
+            class="table table-bordered table-striped table-hover table-sm"
+          >
+            <thead>
+              <tr>
+                <th class="w-50">n</th>
+                <th>2<sup>n</sup></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>0</td>
+                <td>1</td>
+              </tr>
+              <tr v-for="n in 16" :key="n + 'id'">
+                <td>{{ n }}</td>
+                <td>{{ Math.pow(2, n).toLocaleString() }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="col-12 col-sm-6">
+          <table
+            class="table table-bordered table-striped table-hover table-sm"
+          >
+            <thead>
+              <tr>
+                <th class="w-50">n</th>
+                <th>2<sup>n</sup></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="m in 17" :key="m + 'id'">
+                <td>{{ m + 10 }}</td>
+                <td>{{ Math.pow(2, m + 10).toLocaleString() }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   </div>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      step: 6
-    }
-  }
-}
-</script>
