2d299cfd587df3b4531c171594bdefe47f574761
mccrush <mccrush2027@gmail.com>
2021-04-22 10:56:20 +0700

    Settings view poryadok

--- src/views/Logic.vue
+++ src/views/Logic.vue
@@ -36,34 +36,45 @@
 
       <hr />
       <h4>Порядок выполнения логических операций</h4>
-      <div class="row">
-        <div class="col-12 col-sm-6">
-          <a
-            data-fancybox="gallery"
-            href="img/logic/por1.jpg"
-            class="card h-100 mt-2 mt-sm-0"
-          >
-            <img
-              src="img/logic/por1.jpg"
-              alt="Порядок выполнения логических операций"
-              class="w-100"
-            />
-          </a>
-        </div>
-        <div class="col-12 col-sm-6 mt-2 mt-sm-0">
-          <a
-            data-fancybox="gallery"
-            href="img/logic/por2.png"
-            class="card h-100 mt-2 mt-sm-0"
-          >
-            <img
-              src="img/logic/por2.png"
-              alt="Порядок выполнения логических операций"
-              class="w-100"
-            />
-          </a>
-        </div>
-      </div>
+
+      <table
+        class="table table-bordered table-striped table-hover table-sm text-center mb-0"
+      >
+        <thead>
+          <tr>
+            <th>0</th>
+            <td>( )</td>
+            <td class="text-start ps-2 w-75">действия в скобках</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>1</th>
+            <td><span class="overline">X</span></td>
+            <td class="text-start ps-2">инверсия (отрицание)</td>
+          </tr>
+          <tr>
+            <th>2</th>
+            <td>&</td>
+            <td class="text-start ps-2">конъюнкция</td>
+          </tr>
+          <tr>
+            <th>3</th>
+            <td>v</td>
+            <td class="text-start ps-2">дизъюнкция</td>
+          </tr>
+          <tr>
+            <th>4</th>
+            <td>&rarr;</td>
+            <td class="text-start ps-2">импликация (следование)</td>
+          </tr>
+          <tr>
+            <th>5</th>
+            <td>&harr;</td>
+            <td class="text-start ps-2">эквивалентность</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
