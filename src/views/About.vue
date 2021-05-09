750a9b7ac894c0fc769ca3378ea9916ef4b5ca58
mccrush <mccrush2027@gmail.com>
2021-05-09 20:44:46 +0700

    Add Product Hunt button

--- src/views/About.vue
+++ src/views/About.vue
@@ -20,6 +20,26 @@
           >Github</a
         >
       </p>
+      <div class="row">
+        <div class="col-12 col-sm-6 lh-1">
+          <small class="lh-1"
+            >Вы можете оставить голос за данный справочник на сайте Product
+            Hunt. Это поможет узнать о нем большему количеству людей</small
+          >
+        </div>
+        <div class="col-12 col-sm-6">
+          <a
+            href="https://www.producthunt.com/posts/info-math?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-info-math"
+            target="_blank"
+            ><img
+              src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=295252&theme=dark"
+              alt="Info 🎓 Math - справочник по Информатике и Математике | Product Hunt"
+              style="width: 250px; height: 54px"
+              width="250"
+              height="54"
+          /></a>
+        </div>
+      </div>
       <!-- <p>
         Проект Info Math абсолютно бесплатен и всегда таким будет. Но вы можете
         поддержать команду разработчиков на Patreon
